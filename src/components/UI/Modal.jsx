ea61fe1bda24c61778ef238f2314511d48baf4ed
Chisom <chisomije92@gmail.com>
2022-01-10 16:38:12 +0100

    finish project

---
+++ src/components/UI/Modal.jsx
@@ -0,0 +1,41 @@
+import { Fragment } from "react/cjs/react.development";
+import reactDom from "react-dom";
+import classes from "./Modal.module.css";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes["modal--container"]}>
+        <h3>Delete comment</h3>
+        <p>
+          Are you sure you want to delete this comment? This will delete this
+          comment and can't be undone
+        </p>
+        <div className={classes["button-group"]}>
+          <button onClick={props.onCancel}>NO, CANCEL</button>
+          <button onClick={props.onConfirm}>YES, DELETE</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById("overlays");
+
+const Modal = (props) => {
+  return (
+    <Fragment>
+      {reactDom.createPortal(<Backdrop />, portalElement)}
+      {reactDom.createPortal(
+        <ModalOverlay onCancel={props.onCancel} onConfirm={props.onConfirm} />,
+        portalElement
+      )}
+    </Fragment>
+  );
+};
+
+export default Modal;
